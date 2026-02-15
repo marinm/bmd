9acac62e1204f3d2967965c64dc0d5e81388ed61
Marin <marinmema@live.ca>
2026-02-14 20:26:51 -0500

    add blank Paper, use Prettier

---
+++ src/components/Paper.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="paper">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.paper {
+  width: 8.5in;
+  height: 11in;
+  background-color: white;
+  box-shadow: 1mm 1mm 1mm 0 gray;
+  box-sizing: border-box;
+  padding: 1in;
+}
+</style>
