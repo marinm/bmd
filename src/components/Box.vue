fe64f1c923f5893cd1d199c48dc59d45356a3468
Marin <marinmema@live.ca>
2026-02-14 21:08:35 -0500

    show BoxTools on hover

--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,17 +1,48 @@
 <template>
-  <div class="box">
+  <div
+    class="box"
+    :class="{ selected: showBoxTools }"
+    @mouseenter="onMouseEnter"
+    @mouseleave="onMouseLeave"
+  >
+    <BoxTools v-if="showBoxTools" />
     <slot></slot>
   </div>
 </template>
 
+<script lang="ts">
+import BoxTools from "./BoxTools.vue";
+
+export default {
+  components: {
+    BoxTools,
+  },
+  data() {
+    return {
+      showBoxTools: false,
+    };
+  },
+  methods: {
+    onMouseEnter() {
+      this.showBoxTools = true;
+    },
+    onMouseLeave() {
+      this.showBoxTools = false;
+    },
+  },
+};
+</script>
+
 <style scoped>
 .box {
+  display: relative;
   width: 100%;
   height: 100%;
   display: flex;
   flex-direction: column;
   box-sizing: border-box;
   flex-grow: 1;
+  position: relative;
   outline: 1px solid blue;
 }
 </style>
---
+++ src/components/BoxTools.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box-tools"></div>
+</template>
+
+<style scoped>
+.box-tools {
+  position: absolute;
+  bottom: -0.3in;
+  right: 0;
+  width: 1in;
+  height: 0.25in;
+  background-color: white;
+  border: 1px solid blue;
+  box-shadow: 0.1mm 0.1mm 1mm 0 aliceblue;
+  box-sizing: border-box;
+}
+</style>
